1ed561592de8cda07e5ba6828617dab6bbac5362
Jean-François Morin <ioannes.franciscus@gmail.com>
2026-07-12 22:12:50 -0400

    Added missing listing 10.45

---
+++ app/src/test/java/dop/chapter10/Listing10_45.java
@@ -0,0 +1,32 @@
+package dop.chapter10;
+
+public class Listing10_45 {
+
+  /**
+   * ───────────────────────────────────────────────────────
+   * Listing 10.45
+   * ───────────────────────────────────────────────────────
+   * The input squeezes out all other concerns
+   * ───────────────────────────────────────────────────────
+   */
+  class Ingestor {
+    // attributes, constructor, etc. 
+
+    public void ingest(Manifest manifest) {
+        // do work here.
+    }
+}
+
+
+
+
+
+
+
+
+
+  interface Manifest {
+    Iterable<String> values();
+  }
+
+}
